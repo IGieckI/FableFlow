68d5543c1a4ce83b324769203a8abffbc4d87254
IGieckI <giacomo.antonelli3@gmail.com>
2023-12-11 11:49:28 +0100

    Fix GUID var type

--- Sql/Users.sql
+++ Sql/Users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     username VARCHAR(255) PRIMARY KEY,
     password VARCHAR(100) NOT NULL,
-    icon GUID,
+    icon VARCHAR(36), /*uuid()*/
     description VARCHAR(50) DEFAULT ''
 );
