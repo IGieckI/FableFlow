0636b3d450120e33d46b8cd5e9e9f17bab87950b
Dario <dariobekic91@gmail.com>
2023-12-12 08:49:56 +0100

    modified prototype of home footer header

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,3 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>    
+</head>
+<body>
 <?php include 'header.php'; ?>
-
+<main>
+    <p> main </p>
+</main>
 <?php include 'footer.php'; ?>
+</body>
+</html>
