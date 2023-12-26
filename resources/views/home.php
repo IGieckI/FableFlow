90a2cc7339f49e08d7466c83e3f582cf9dd5e963
IGieckI <giacomo.antonelli3@gmail.com>
2023-12-26 12:09:47 +0100

    Add header references to home.php

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,29 +1,19 @@
 <!DOCTYPE HTML>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <title>Home</title>   
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="it">
     <head>
-        <meta charset="UTF-8">>
-        <title>TITLE</title>
+        <meta charset="UTF-8">
+        <title>Home</title>   
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="css/header.css">
+        <title>Home</title>
     </head>
-
-    <?php include 'header.php'; ?>
-<main>
-    <p> main </p>
-</main>
     <body>
+        <?php include 'header.php'; ?>
         <main>
             <p> Content </p>
         </main>
-
         <?php include 'footer.php'; ?>
-</body>
-</html>
     </body>
 </html>
