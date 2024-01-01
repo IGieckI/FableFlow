e98560525248582f11ab95f7011ad61fbe4d492f
IGieckI <giacomo.antonelli3@gmail.com>
2024-01-02 00:22:30 +0100

    Add hover effect to homepage post

--- resources/views/home.php
+++ resources/views/home.php
@@ -23,12 +23,11 @@
             <div class="main-container">
                 <div class="row justify-content-center">
                     <div class="col-lg-8">
-                        <!-- Sample Post 1 -->
-                        <div class="post-container">
+                        <div class="post-container" onclick="window.location.href='postPage.php';">
                             <div class="container">
                                 <div class="row user-info">
                                     <div class="col-10">
-                                        <img src="user-icon.png" alt="User Icon" width="30" height="30"> <!-- Replace with actual user icon -->
+                                        <img src="user-icon.png" alt="User Icon" width="30" height="30">
                                         <span>Username</span>
                                     </div>
                                     <div class="col-2">
@@ -44,8 +43,7 @@
                                         <span><i class="bi bi-chat-dots"></i> 10</span>
                                         <span><i class="bi bi-fire"></i> 20</span>
                                     </div>
-                                </div>                
-
+                                </div>
                                 <div class="row post-content">
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis commodo odio aenean sed adipiscing diam donec. Egestas congue quisque egestas diam in arcu cursus euismod. Duis ut diam quam nulla porttitor massa. Maecenas pharetra convallis posuere morbi leo. Morbi tincidunt ornare massa eget egestas purus viverra accumsan. 
                                 </div>
@@ -56,6 +54,5 @@
             </div>
         </main>
         <?php include 'footer.php'; ?>
-    </body>    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
 </html>
