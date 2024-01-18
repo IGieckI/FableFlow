8c876577086944f45bce8934cdeff02150b0ec63
IGieckI <giacomo.antonelli3@gmail.com>
2024-01-18 17:02:50 +0100

    Fix php Post objects creation

--- resources/db/get_posts.php
+++ resources/db/get_posts.php
@@ -1,6 +1,7 @@
 <?php
 require('config.php');
 require('dbhelper.php');
+require('masks/post.php');
 
 $db = new DbHelper(HOST, USER, PASS, DB, PORT, SOCKET);
 
@@ -16,7 +17,20 @@ $start = ($page - 1) * $postsPerPage;
 
 try {
     // Fetch posts from the database (replace with your actual database query)
-    $result = $db->findBy([], $postsPerPage, $start, Tables::Stories);
+    $chapters = $db->findBy([], $postsPerPage, $start, Tables::Chapters);
+
+    foreach ($chapters as $chapter) {
+        $story = $db->findBy(['story_id' => $chapter['story_id']], null, null, Tables::Stories);
+        $story = $story[0];
+        $user = $db->findBy(['username' => $story['username']], null, null, Tables::Users);
+        $user = $user[0];
+        $likes = $db->count(['chapter_id' => $chapter['chapter_id']], Tables::Likes);
+        $likes = $likes[0]['COUNT(*)'];
+        $comments = $db->count(['comment_id' => $chapter['chapter_id']], Tables::Comments);
+        $comments = $comments[0]['COUNT(*)'];
+
+        $result[] = new Post($user['icon'], $user['username'], $chapter['publication_datetime'], $story['title'], $comments, $likes, $chapter['content']);
+    }
 
     // Set the Content-Type header
     header('Content-Type: application/json');
