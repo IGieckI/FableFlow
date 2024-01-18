759e2c7f9037e2ff76712cf914e24d71e80d1952
IGieckI <giacomo.antonelli3@gmail.com>
2024-01-18 17:03:48 +0100

    Remove incapsulation from Post class (cant be used after AJAX trasmission)

--- resources/db/masks/post.php
+++ resources/db/masks/post.php
@@ -1,11 +1,11 @@
 <?php
     class Post {
 
-        private $user_icon, $username, $time;
-        private $post_title, $num_comments, $num_likes;
-        private $post_content;
+        public $user_icon, $username, $time;
+        public $post_title, $num_comments, $num_likes;
+        public $post_content;
         
-        public function __construct($user_icon, $username, $time, $post_title, $num_comments, $num_likes, $post_content) {) {            
+        public function __construct($user_icon, $username, $time, $post_title, $num_comments, $num_likes, $post_content) {            
             $this->user_icon = $user_icon;
             $this->username = $username;
             $this->time = $time;
@@ -13,34 +13,6 @@
             $this->num_comments = $num_comments;
             $this->num_likes = $num_likes;
             $this->post_content = $post_content;
-        }
-
-        public function get_user_icon() {
-            return $this->user_icon;
-        }
-
-        public function get_username() {
-            return $this->username;
-        }
-
-        public function get_time() {
-            return $this->time;
-        }
-
-        public function get_post_title() {
-            return $this->post_title;
-        }
-
-        public function get_num_comments() {
-            return $this->num_comments;
-        }
-
-        public function get_num_likes() {
-            return $this->num_likes;
-        }
-
-        public function get_post_content() {
-            return $this->post_content;
         }
     }
 ?>
