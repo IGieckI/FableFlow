652708fedc0547924350327c8d4202deb7f2c006
IGieckI <giacomo.antonelli3@gmail.com>
2024-01-24 00:46:17 +0100

    Create Comments.php as subpage for Post page

---
+++ src/views/post/content/Comments.php
@@ -0,0 +1,9 @@
+<div class="main-container">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div id="comments-container">
+                <!-- Comments will be loaded here -->
+            </div>                        
+        </div>
+    </div>
+</div>
