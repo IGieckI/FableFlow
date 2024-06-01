405e4702462f5d933f98ba74ef4b12d2ae50c1e8
Dario <dariobekic91@gmail.com>
2024-06-01 17:23:13 +0200

    added styles

--- src/server/api/UpdateBio.php
+++ src/server/api/UpdateBio.php
@@ -4,7 +4,10 @@ require __DIR__ . '/../utilities/DbHelper.php';
 
 $db = new DbHelper(HOST, USER, PASS, DB, PORT, SOCKET);
 
-$db->update(['description'=>"'".$_POST['newbio']."'"], [], Tables::Users);
+if (isset($_SESSION['LOGGED'])) {
+    $db->update(['description'=>"'".$_POST['newbio']."'"], ['username'=>"'".$_SESSION['username']."'"], Tables::Users);
+}
+
 
 $db->disconnect();
 
